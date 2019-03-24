de171bd3c704abb02e1fa1e213249f2b75d65875
Marcel Dempers <aimvec@gmail.com>
2019-03-24 12:32:14 +1100

    storage class for writing files to couchdb

--- components/music-consumer/src/storage.go
+++ components/music-consumer/src/storage.go
@@ -2,27 +2,65 @@ package main
 
 
 import (
+	"context"
 	"fmt"
-	"github.com/flimzy/kivik"
-    _ "github.com/go-kivik/couchdb"
-	"time"
+	"github.com/go-kivik/kivik"
+     _ "github.com/go-kivik/couchdb"
 	"github.com/rs/xid"
 	"bytes"
+	"io"
 )
+
+type ClosingBuffer struct { 
+	*bytes.Reader 
+} 
+
+func (cb *ClosingBuffer) Close() (err error) { 
+	//we don't actually have to do anything here, since the buffer is 
+	//just some data in memory 
+	//and the error is initialized to no-error 
+	return 
+} 
 
 func ConnectAndSaveContent(filename string, content []byte) {
 
-	var timeout = time.Duration(500 * time.Millisecond)
-	conn, err := couchdb.NewConnection("music-storage",5984,timeout)
-	auth := couchdb.BasicAuth{Username: "user", Password: "password" }
-	db := conn.SelectDB("mydatabase", &auth)
+	//var timeout = time.Duration(500 * time.Millisecond)
+	//conn, err := couchdb.NewConnection("music-storage",5984,timeout)
+	//auth := couchdb.BasicAuth{Username: "user", Password: "password" }
+	//db := conn.SelectDB("mydatabase", &auth)
+	
+	client, err := kivik.New("couch", "http://music-storage:5984/")
+	if err != nil {
+        panic(err)
+    }
+	
+	db := client.DB(context.TODO(), "mydatabase")
+ 
 	id := xid.New()
 	reader := bytes.NewReader(content)
-
-	rev, err := db.Save("empty", id.String(), "") 
-	rev, err = db.SaveAttachment(id.String(), "", "test" , "Application/audio", reader )
-	fmt.Println(rev)
-	if err != nil {
-		panic(err)
+	
+	doc := map[string]interface{}{
+        "_id":      id.String(),
+        "filename":     filename,
 	}
+	
+	cb := &ClosingBuffer{reader}
+	var rc io.ReadCloser
+	rc = cb 
+	defer rc.Close()
+	attachment := &kivik.Attachment{Filename : filename, Content : rc, ContentType : "audio/mpeg" }
+
+	rev, err := db.Put(context.TODO(), id.String(), doc)
+    if err != nil {
+        panic(err)
+	}
+
+	rev, err = db.PutAttachment(context.TODO(),id.String(), rev, attachment)
+	if err != nil {
+        panic(err)
+	}
+	
+	//rev, err := db.Save("empty", id.String(), "") 
+	//rev, err = db.SaveAttachment(id.String(), "", "test" , "Application/audio", reader )
+	fmt.Println(rev)
 }
