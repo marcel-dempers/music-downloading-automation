81cfc63e670970da46e8586c11c43f6b2228cb15
Marcel Dempers <aimvec@gmail.com>
2019-08-15 15:28:12 +1000

    option to allow cors for development

--- components/music-request-api/src/main.go
+++ components/music-request-api/src/main.go
@@ -6,14 +6,27 @@ import (
 	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
 	"fmt"
-	//"app/models"
+	"os"
 )
+
+var environment = os.Getenv("ENVIRONMENT")
+
+func cors(writer http.ResponseWriter) () {
+	if(environment == "DEBUG"){
+		writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-MY-API-Version")
+		writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		writer.Header().Set("Access-Control-Allow-Origin", "*")
+	}
+}
+
+
 func main() {
 	config := GetConfiguration()
 	router := httprouter.New()
 
 	router.POST("/submit/*query", func(w http.ResponseWriter, r *http.Request, p httprouter.Params){
-		 
+		cors(w) 
 		err := SubmitRequest(w,r,p,config)
 
 		if(err != nil){
